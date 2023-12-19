5b1a518ee781c920f93dcac7f64606dc350360d1
khosro moradi <moradikhosro89@gmail.com>
2023-12-19 05:20:13 +0330

    initial dark mode in app was deactivated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { returnMySourceFont } from "@/helpers/returnMySourceFont";
 
 const vazirMatn = localFont({
   src: [
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex text-9xl font-black min-h-screen flex-col items-center justify-between p-24">
+    <main className="">
       <div>خسرو مرادی</div>
     </main>
   );
