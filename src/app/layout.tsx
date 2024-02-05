f9d171f346547901153dc8f46da37a06b9eb59dd
khosro moradi <moradikhosro89@gmail.com>
2024-02-06 00:03:16 +0330

    cleaning code was done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,60 +1,10 @@
 "use client";
-import localFont from "next/font/local";
 import "./globals.css";
 import StoreProvider from "./StoreProvider";
 import { makeStore } from "@/ReduxToolkit/store";
 import Header from "@/components/modules/shared/Header";
 import Footer from "@/components/modules/shared/Footer";
-
-const vazirMatn = localFont({
-  src: [
-    {
-      path: "../../public/font/Vazirmatn-Thin.woff2",
-      weight: "100",
-      style: "normal",
-    },
-    {
-      path: "../../public/font/Vazirmatn-ExtraLight.woff2",
-      weight: "200",
-      style: "normal",
-    },
-    {
-      path: "../../public/font/Vazirmatn-Light.woff2",
-      weight: "300",
-      style: "normal",
-    },
-    {
-      path: "../../public/font/Vazirmatn-Regular.woff2",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../../public/font/Vazirmatn-Medium.woff2",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "../../public/font/Vazirmatn-SemiBold.woff2",
-      weight: "600",
-      style: "normal",
-    },
-    {
-      path: "../../public/font/Vazirmatn-Bold.woff2",
-      weight: "700",
-      style: "normal",
-    },
-    {
-      path: "../../public/font/Vazirmatn-ExtraBold.woff2",
-      weight: "800",
-      style: "normal",
-    },
-    {
-      path: "../../public/font/Vazirmatn-Black.woff2",
-      weight: "900",
-      style: "normal",
-    },
-  ],
-});
+import { vazirMatn } from "@/helpers/vazirMatnFont";
 
 export default function RootLayout({
   children,
---
+++ src/helpers/vazirMatnFont.tsx
@@ -0,0 +1,51 @@
+import localFont from "next/font/local";
+
+export const vazirMatn = localFont({
+    src: [
+      {
+        path: "../../public/font/Vazirmatn-Thin.woff2",
+        weight: "100",
+        style: "normal",
+      },
+      {
+        path: "../../public/font/Vazirmatn-ExtraLight.woff2",
+        weight: "200",
+        style: "normal",
+      },
+      {
+        path: "../../public/font/Vazirmatn-Light.woff2",
+        weight: "300",
+        style: "normal",
+      },
+      {
+        path: "../../public/font/Vazirmatn-Regular.woff2",
+        weight: "400",
+        style: "normal",
+      },
+      {
+        path: "../../public/font/Vazirmatn-Medium.woff2",
+        weight: "500",
+        style: "normal",
+      },
+      {
+        path: "../../public/font/Vazirmatn-SemiBold.woff2",
+        weight: "600",
+        style: "normal",
+      },
+      {
+        path: "../../public/font/Vazirmatn-Bold.woff2",
+        weight: "700",
+        style: "normal",
+      },
+      {
+        path: "../../public/font/Vazirmatn-ExtraBold.woff2",
+        weight: "800",
+        style: "normal",
+      },
+      {
+        path: "../../public/font/Vazirmatn-Black.woff2",
+        weight: "900",
+        style: "normal",
+      },
+    ],
+  });
